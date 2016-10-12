1654a8988eb6167a076a5f0b62e44406be99dd92
Mohsen-Malekan <mhs.malekan@gmail.com>
2016-10-12 18:25:04 +0330

    add landing page

--- server/routes.js
+++ server/routes.js
@@ -14,18 +14,26 @@ export default function(app) {
 
   app.use('/auth', require('./auth').default);
 
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+  app.route('/')
+    .get((req, res) => {
+      console.log('>>>>>>>> /');
+      res.redirect('/landing');
+    });
 
   app.route('/landing')
     .get((req, res) => {
+      console.log('>>>>>>>> /landing');
       res.sendFile(path.resolve(`${app.get('appPath')}/landing.html`));
     });
+
+  // All undefined asset or api routes should return a 404
+  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+    .get(errors[404]);
 
   // All other routes should redirect to the index.html
   app.route('/*')
     .get((req, res) => {
+      console.log('>>>>>>>> /*');
       res.sendFile(path.resolve(`${app.get('appPath')}/index.html`));
     });
 }
