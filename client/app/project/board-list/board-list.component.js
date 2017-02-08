e944b06ccb11e1b9bfc5ee1165448c9b4858eed7
MortezaFathnia <fathniamorteza@gamil.com>
2017-02-08 22:58:24 +0330

    bords list bug fixed.

--- client/app/project/board-list/board-list.component.js
+++ client/app/project/board-list/board-list.component.js
@@ -5,13 +5,20 @@ import uiRouter from 'angular-ui-router';
 export class boardListComponent {
   constructor($rootScope, appConfig, Auth, ProjectAuth) {
     'ngInject';
-    filterBoards();
+    let currentUser = Auth.getCurrentUserSync();
+    this.allBoards = appConfig.boards;
+    if(this.onlyProjectBoards) {
+      let isAdmin = ProjectAuth.hasAccess(this.project, 'admin');
+      let lookup = _.keyBy(this.project.boards, b => b.name);
+      if(!isAdmin) {
+        lookup = _.filter(this.project.boards, b => _.findIndex(b.users, {_id: currentUser._id}) > -1);
+        lookup = _.keyBy(lookup, b => b.name);
+      }
+      this.allBoards = _.filter(this.allBoards, b => lookup[b.name] !== undefined);
+    }
+    this.allBoards = _.groupBy(this.allBoards, b => b.category);
 
     $rootScope.$on('PROJECT_UPDATED', () => {
-      filterBoards();
-    });
-
-    function filterBoards() {
       let currentUser = Auth.getCurrentUserSync();
       this.allBoards = appConfig.boards;
       if(this.onlyProjectBoards) {
@@ -24,7 +31,7 @@ export class boardListComponent {
         this.allBoards = _.filter(this.allBoards, b => lookup[b.name] !== undefined);
       }
       this.allBoards = _.groupBy(this.allBoards, b => b.category);
-    }
+    });
   }
 }
 
